9d19956e2ed3366415ff00f84331ef9fc4c98a00
Serge <53587568+sanabae3@users.noreply.github.com>
2023-02-22 07:18:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
-# Configure the AWS Provider
+# https://registry.terraform.io/providers/hashicorp/aws/3.74.2
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "= 3.74.2"
+    }
+  }
+}
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"
+  access_key = "AKIAQLO5FEV5LFXRKC5D"
+  secret_key = "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"
 }
 
 # Create a VPC
